7074e1667f32bce54faac57180b71afd162edc4d
Pere <pere.solaclaver@gmail.com>
2019-11-12 21:11:28 +0000

    react router dom and add link to registration form

--- nice-app/src/App.js
+++ nice-app/src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
-import "./App.css";
+import { Link, Route } from "react-router-dom";
 
 // COMPONENTS
 
 import AuthButton from "./components/authButton";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
-  return <div className="App">
-    <AuthButton text={"Login"}/>
-    <AuthButton text={"Logout"}/>
-  </div>;
+  return (
+    <div className="App">
+      <Link to="/auth/register">Register</Link>
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <AuthButton {...props} text={"Login"} />;
+        }}
+      />
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <AuthButton {...props} text={"Logout"} />;
+        }}
+      />
+
+      <Route path="/auth/register" component={RegistrationForm} />
+    </div>
+  );
 }
 
 export default App;
---
+++ nice-app/src/components/RegistrationForm.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RegistrationForm(props) {
+  return <div>Hello from registration form</div>;
+}
